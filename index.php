c4de94bfe918233018679d64453567365f45022a
rogasian <rogasianmvungi@gmail.com>
2025-09-14 14:34:46 +0300

    pdo database connection

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php 
 require_once 'config/db.php';
 
+// $insert = "insert into users(name) values(?)";
+$insert = "insert into users (name) values(:name)";
+// $sql = "insert into users(name) values('John')";
+
+$stm = $conn -> prepare($insert);
+// $stm -> execute(['Haji' ]);
+$stm ->execute(['name' => 'Anda Mbillah']);
+
